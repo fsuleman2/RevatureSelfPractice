7bc00f76e829dd047d87d42771abbee09c9af755
fsuleman2 <sydsulmn7@gmail.com>
2021-07-03 00:16:07 +0530

    1stjuly task and 3 arrays prblms added

---
+++ July1st_Task/src/task1/LastCharacterToUpperCase.java
@@ -0,0 +1,19 @@
+package task1;
+
+public class LastCharacterToUpperCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hello hi good evening everyone hope you are enjoying progamming with java";
+		StringBuilder sb=new StringBuilder();
+		String arr[] = s.split(" ");
+		for (int i = 0; i < arr.length; i++) {
+//			System.out.println(arr[i]);
+			int j = arr[i].length()-1;
+			//System.out.println(j);
+			sb.append(arr[i].substring(0,arr[i].length()-1)).append(Character.toUpperCase(arr[i].charAt(j))).append(" ");
+		}
+		System.out.println(sb.toString().trim());
+	}
+
+}
